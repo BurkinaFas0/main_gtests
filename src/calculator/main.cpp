7ed86c1b914cb75831f4bb880896746f37fd9420
BurkinaFas0 <lebus.spb@gmail.com>
2026-03-28 04:15:02 +0300

    Add artifact and release yml

---
+++ src/calculator/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "calculator.h"  // предположим, есть такой заголовок
+
+int main() {
+    std::cout << "Калькулятор УСПЕШНО запущен!"  << std::endl;
+    // здесь можно сделать простой REPL или просто проверку
+    return 0;
+}
